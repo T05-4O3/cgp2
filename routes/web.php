a62212fc5ffde6fd3ff8e4792999eeff9cb8eb7b
T05-4O3 <yohsuke65@hotmail.com>
2023-09-07 15:35:22 +0900

    make admin LoginLogoutCRUD(NoblUIbase)

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::middleware(['auth','role:admin'])->group(function(){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 }); // End Group Admin Middleware
 
 Route::middleware(['auth','role:creator'])->group(function(){
